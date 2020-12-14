3a4f579707a97d0629b3912348f2ccb988496b25
David Nick <dcnarchitecture@gmail.com>
2020-12-14 13:24:40 -0700

    second commit

--- rental_hub/urls.py
+++ rental_hub/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login', views.login),
     path('logout', views.logout),
     path('main', views.main),
-    path('/markets', views.markets)
+    # path('market/<int:market_id>', views.markets)
 ]
--- rental_hub/views.py
+++ rental_hub/views.py
@@ -1,22 +1,79 @@
 from django.shortcuts import render, redirect, HttpResponse
-from 
+from django.contrib import messages
+from .models import User, UserManager
+import bcrypt
 
-def index
-    pass
+# CREATE
+def index(request):
+    return render(request, 'index.html')
 
-def register
-    pass
+def main(request):
+    if 'user_id' not in request.session:
+        return redirect('/main')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user
+    }
+    return render(request, 'main.html', context)
 
-def login
-    pass
+def register(request):
+    if request.method == "GET":
+        return redirect('/')
+    errors = User.objects.validate(request.POST)
+    if errors:
+        for e in errors.values():
+            messages.error(request, e)
+        return redirect('/')
+    else:
+        new_user = User.objects.register(request.POST)
+        request.session['user_id'] = new_user.id
+        return redirect('/main')
 
-def logout
-    pass
+# READ
 
-def main
-    pass
+def login(request):
+    if request.method == "GET":
+        return redirect('/login')
+    if not User.objects.authenticate(request.POST['email'], request.POST['password']):
+        messages.error(request, 'Invalid Email/Password')
+        return redirect('/login')
+    user = User.objects.get(email=request.POST['email'])
+    request.session['user_id'] = user.id
+    return redirect('/main')
 
-def markets
-    pass
+def logout(request):
+    request.session.clear()
+    return redirect('/')
 
-# Create your views here.
+
+
+# def markets
+#     pass
+
+# UPDATE
+
+# def update(request):
+#     if 'user_id' not in request.session:
+#         return redirect('/')
+#     user = User.objects.get(id=request.session['user_id'])
+#     context = {
+#         'user': user
+#     }
+#     return render(request, 'myaccount.html', context)
+
+# def create_quote(request):
+#     if request.method == 'POST':
+#         errors = Quote.objects.validate(request.POST)
+#         if errors:
+#             for e in errors.values():
+#                 messages.error(request, e)
+#             return redirect('/quotes')
+#         quote = Quote.objects.create(author=request.POST['author'],quote_field=request.POST['content'], poster=User.objects.get(id=request.session['user_id']))
+#     return redirect('/quotes')
+
+# DELETE
+
+# def delete_market(request):
+#         market = Market.objects.filter().delete
+#         return redirect('/profile')
+
